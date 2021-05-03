e6ed12595655feb6e0959b2976c106b1aa8f54b4
indranil2507 <iindranilsaha1@gmail.com>
2021-05-03 17:12:42 +0530

    footer

--- calip-front/src/components/footer.jsx
+++ calip-front/src/components/footer.jsx
@@ -1,61 +1,57 @@
-import React from 'react';
-import './footer.css';
+import React from "react";
+import "./footer.css";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import {
   FaFacebook,
   FaInstagram,
   FaYoutube,
   FaTwitter,
-  FaLinkedin
-} from 'react-icons/fa';
-import { MdFingerprint } from 'react-icons/md';
+  FaLinkedin,
+} from "react-icons/fa";
+import { MdFingerprint } from "react-icons/md";
 
 function Footer() {
   return (
-    <div className='footer-container'>
-     
-     
-      <section className='social-media'>
-        <div className='social-media-wrap'>
-          <div className='footer-logo'>
-            <Link to='/' className='social-logo'>
+    <div className="footer-container">
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
               CALIP
             </Link>
           </div>
-          <small className='website-rights'>Calip © 2021</small>
-          <div className='social-icons'>
+          <small className="website-rights">Calip © 2021</small>
+          <div className="social-icons">
             <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
             >
               <FaFacebook />
             </Link>
             <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
             >
               <FaInstagram />
             </Link>
             <Link
-              className='social-icon-link'
-              to={
-                '/'
-              }
-              target='_blank'
-              aria-label='Youtube'
+              className="social-icon-link"
+              to={"/"}
+              target="_blank"
+              aria-label="Youtube"
             >
               <FaYoutube />
             </Link>
             <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Twitter"
             >
               <FaTwitter />
             </Link>
@@ -65,7 +61,9 @@ function Footer() {
               target='_blank'
               aria-label='LinkedIn'
             > */}
-             <a href="https://www.linkedin.com/company/calip/" target='_blank' ><FaLinkedin/></a> 
+            <a href="https://www.linkedin.com/company/calip/" target="_blank">
+              <FaLinkedin />
+            </a>
             {/* </Link> */}
           </div>
         </div>

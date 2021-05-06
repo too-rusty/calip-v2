5d4ae5acf5b791a57eb09e07525af4cebf53bc18
indranil2507 <iindranilsaha1@gmail.com>
2021-05-06 19:18:55 +0530

    view-design

--- calip-front/src/components/cchain.js
+++ calip-front/src/components/cchain.js
@@ -1,192 +1,222 @@
 import React from "react";
-import axios from 'axios';
-import Cookies from 'universal-cookie';
+import axios from "axios";
+import Cookies from "universal-cookie";
 import { Redirect } from "react-router";
 import CChainView from "./cchainView";
 import { Link } from "react-router-dom";
-import './cchain.css';
-import profileImage from '../images/profilephoto.jpg'
+import "./cchain.css";
+import profileImage from "../images/profilephoto.jpg";
 
 class Cchain extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            ccid : this.props.match.params.ccid,
-            cardchain : null,
-            editmode : false,
-        }
-        console.log("STATE CC", this.state.ccid)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      ccid: this.props.match.params.ccid,
+      cardchain: null,
+      editmode: false,
+    };
+    console.log("STATE CC", this.state.ccid);
+  }
 
-
-    bookmarkFUNC(){
-        const cookies = new Cookies();
-        let tokk = cookies.get('calip_token')
-        const config = {
-            headers : {
-                'token' : tokk
-            }
-        }
-        let uname = cookies.get('calip_uname')
-        console.log('uname isssss',uname)
-        let data = {
-            "username" : uname,
-            "ccid" : this.state.cardchain.ccid,
-            "save": this.state.cardchain.bookmarked
-        }
-        let url = 'http://localhost:8000/bookmark/createORremove'
-        axios.post(url, JSON.stringify(data), config)
-        .then(
-            res => {
-                console.log('calling bookmark api')
-                console.log("returned",res.data)
-            }
-        ).catch(
-            error => {
-                console.log('tis he')
-                if (error.response) {
-                    console.log("error bookmark", error.response)
-                } else {
-                    console.log('Show error notification!')
-                }
-            }
-        )
-    }
-
-
-    // _isMounted = true
-    componentDidMount(){
-        this._isMounted = true
-        const data = {}
-        // dont know hot to pass it so using cookie for now
-        const cookies = new Cookies();
-        let tokk = cookies.get('calip_token')
-        const config = {
-            headers : {
-                'token' : tokk
-            }
-        }
-        console.log("config",config)
-        let url = 'http://localhost:8000/cc/' + this.state.ccid.toString()
-        axios.get(url, config)
-        .then(
-            res => {
-                console.log("called fetch API and got DATA -> ", res.data)
-                if(res.data.ccid != undefined) {
-                    if (res.data.draft === true ) this.setState({cardchain:"NOTFOUND"})
-                    else {
-                        this.setState({cardchain:res.data})
-                    }
-                }
-            }
-        ).catch(
-            error => {
-                console.log('tis he')
-                if (error.response) {
-                    if(error.response.data.error === "record not found") {
-                        // console.log("YOOOO record not found")
-                        this.setState({cardchain:"NOTFOUND"})
-                    }
-                    // console.log(error.response.data)
-                    // cookies.set('calip_token', 'null', { path: '/' }) // whyyyy , not needed
-                } else {
-                    console.log('Show error notification!')
-                }
-            }
-        )
-
-    }
-    componentWillUnmount() {
-        this._isMounted = false
-    }
-
-    edit() {
-        this.setState({edit:true})
-    }
-    toggleBookmark(e) {
-        e.preventDefault()
-        let ccc = this.state.cardchain
-        ccc.bookmarked = !ccc.bookmarked
-        this.setState({cardchain:ccc})
-        this.bookmarkFUNC()
-    }
-
-    render() {
-        if(this.state.edit) { // TODO change here to edit view component or link
-            return <Redirect to={'/cc/'+this.state.ccid.toString()+'/edit'} />
-        }
-        let view
-        if (parseInt(this.state.ccid).toString() === "NaN") {
-            // REDIRECT
-            view = <div>NOT a number in the url, invalid</div>
-        } else if (this.state.cardchain === null ) {
-            view = <div>Loading ....</div>
-        } else if (this.state.cardchain === "NOTFOUND" ){
-            view = <div>No card</div>
+  bookmarkFUNC() {
+    const cookies = new Cookies();
+    let tokk = cookies.get("calip_token");
+    const config = {
+      headers: {
+        token: tokk,
+      },
+    };
+    let uname = cookies.get("calip_uname");
+    console.log("uname isssss", uname);
+    let data = {
+      username: uname,
+      ccid: this.state.cardchain.ccid,
+      save: this.state.cardchain.bookmarked,
+    };
+    let url = "http://localhost:8000/bookmark/createORremove";
+    axios
+      .post(url, JSON.stringify(data), config)
+      .then((res) => {
+        console.log("calling bookmark api");
+        console.log("returned", res.data);
+      })
+      .catch((error) => {
+        console.log("tis he");
+        if (error.response) {
+          console.log("error bookmark", error.response);
         } else {
-
-            let cc = this.state.cardchain
-            view = (
-                <div>
-                    <div className="view-hero">
-                        <div className="section-1">
-                        <div className="title">{cc.title}</div>
-                        <div className="about-view"> {cc.about}</div>
-                        {/* <div> Likes:{cc.likes}</div> */}
-                        <div className="buttons">
-                        <div>{cc.loggedin ? 
-                        <div>
-                            
-                        {<Bookmark yes={cc.bookmarked} toggleBookmark={this.toggleBookmark.bind(this)} /> }
-                        </div> : <div></div> }
-                        </div>
-                    
-                        
-                         {/* Editable or not */}
-                        <div>
-                        {cc.editable ? 
-                        <div>
-                        {<button onClick = {() => this.edit() }>Edit</button> }
-                        </div> : <div></div> }
-                         </div>
-                        </div>
-                        
-                        
-                        
-                        </div>
-                        <div className="section-2">
-                        <div ><img className="profile-photo" src={profileImage} alt="display image"/></div>
-                        <div className="user"><Link to={`/profile/${cc.username}`}>{cc.username}</Link></div>
-                        </div>
- 
-            </div>
-            <div><CChainView chain={this.state.cardchain}/></div>
-                </div>
-            
-            )
+          console.log("Show error notification!");
         }
-        return (
-            <div>{view}</div>
-        )
-    }
+      });
+  }
 
+  // _isMounted = true
+  componentDidMount() {
+    this._isMounted = true;
+    const data = {};
+    // dont know hot to pass it so using cookie for now
+    const cookies = new Cookies();
+    let tokk = cookies.get("calip_token");
+    const config = {
+      headers: {
+        token: tokk,
+      },
+    };
+    console.log("config", config);
+    let url = "http://localhost:8000/cc/" + this.state.ccid.toString();
+    axios
+      .get(url, config)
+      .then((res) => {
+        console.log("called fetch API and got DATA -> ", res.data);
+        if (res.data.ccid != undefined) {
+          if (res.data.draft === true) this.setState({ cardchain: "NOTFOUND" });
+          else {
+            this.setState({ cardchain: res.data });
+          }
+        }
+      })
+      .catch((error) => {
+        console.log("tis he");
+        if (error.response) {
+          if (error.response.data.error === "record not found") {
+            // console.log("YOOOO record not found")
+            this.setState({ cardchain: "NOTFOUND" });
+          }
+          // console.log(error.response.data)
+          // cookies.set('calip_token', 'null', { path: '/' }) // whyyyy , not needed
+        } else {
+          console.log("Show error notification!");
+        }
+      });
+  }
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  edit() {
+    this.setState({ edit: true });
+  }
+  toggleBookmark(e) {
+    e.preventDefault();
+    let ccc = this.state.cardchain;
+    ccc.bookmarked = !ccc.bookmarked;
+    this.setState({ cardchain: ccc });
+    this.bookmarkFUNC();
+  }
+
+  render() {
+    if (this.state.edit) {
+      // TODO change here to edit view component or link
+      return <Redirect to={"/cc/" + this.state.ccid.toString() + "/edit"} />;
+    }
+    let view;
+    if (parseInt(this.state.ccid).toString() === "NaN") {
+      // REDIRECT
+      view = <div>NOT a number in the url, invalid</div>;
+    } else if (this.state.cardchain === null) {
+      view = <div>Loading ....</div>;
+    } else if (this.state.cardchain === "NOTFOUND") {
+      view = <div>No card</div>;
+    } else {
+      let cc = this.state.cardchain;
+      view = (
+        <div>
+          <div className="view-hero">
+            <div className="section-1">
+              <div className="viewtitle">{cc.title}</div>
+
+              <div className="userSection">
+                <Link to={`/profile/${cc.username}`}>{cc.username}</Link>
+                <div>
+                  <img
+                    className="profile-photo"
+                    src={profileImage}
+                    alt="display image"
+                  />
+                </div>
+              </div>
+
+              {/* <div> Likes:{cc.likes}</div> */}
+
+              <div className="buttons">
+                <div>
+                  {cc.loggedin ? (
+                    <div>
+                      {
+                        <Bookmark
+                          yes={cc.bookmarked}
+                          toggleBookmark={this.toggleBookmark.bind(this)}
+                        />
+                      }
+                    </div>
+                  ) : (
+                    <div></div>
+                  )}
+                </div>
+                <div>
+                  {cc.loggedin ? (
+                    <div>{<button>Like</button>}</div>
+                  ) : (
+                    <div></div>
+                  )}
+                </div>
+
+                {/* Editable or not */}
+                <div>
+                  {cc.editable ? (
+                    <div>
+                      {<button onClick={() => this.edit()}>Edit</button>}
+                    </div>
+                  ) : (
+                    <div></div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="viewinfo">
+            <div className="info">
+              <div>DESCRIPTION</div>
+              <div className="about-view"> {cc.about}</div>
+              <div className="categoryTag">{cc.tags} </div>
+            </div>
+          </div>
+
+          <div>
+            <CChainView chain={this.state.cardchain} />
+          </div>
+        </div>
+      );
+    }
+    return <div>{view}</div>;
+  }
+}
+class Tag extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let tag = this.props.val;
+    return (
+      <div className="searched-tag">{tag}</div> // better if this is in a small box or something
+    );
+  }
 }
 
 class Bookmark extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return (
-            <button onClick={e=>this.props.toggleBookmark(e)}>
-                <div>Bookmarked : {this.props.yes ? "yes ": "no"}</div>
-            </button>
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <button onClick={(e) => this.props.toggleBookmark(e)}>
+        <div>Bookmarked : {this.props.yes ? "yes " : "no"}</div>
+      </button>
+    );
+  }
 }
 
-
-export default Cchain
+export default Cchain;
 
 /*
 
--- calip-front/src/components/cchainView.js
+++ calip-front/src/components/cchainView.js
@@ -54,33 +54,35 @@ class CChainView extends React.Component {
     // ok so the above is the short version of all the cards
     let cidx = this.state.cidx;
     return (
-      <div class="view-section">
-        <div className="full-view">
-          {cidx !== null ? (
-            <div>
-              {/* <div><PreviewCard card={this.state.cards[cidx]}/></div> */}
+      <div>
+        <div class="view-section">
+          <div className="full-view">
+            {cidx !== null ? (
               <div>
-                <DetailViewCard card={this.state.cc[cidx]} />
+                {/* <div><PreviewCard card={this.state.cards[cidx]}/></div> */}
+                <div>
+                  <DetailViewCard card={this.state.cc[cidx]} />
+                </div>
+              </div>
+            ) : (
+              <div>
+                {" "}
+                <DetailViewCard card={this.state.cc[0]} />
+              </div>
+            )}
+            <div>{cards.length === 0 ? "WOOPS! no card" : ""}</div>
+          </div>
+          <div className="chain-view">
+            {/* <Scrollbars style={{width:300,height:600,color:"yellow !important" }}
+                 > */}
+            <div class="scrollbar" id="style-1">
+              <div class="force-overflow">
+                <div>{cards}</div>
               </div>
             </div>
-          ) : (
-            <div>
-              {" "}
-              <DetailViewCard card={this.state.cc[0]} />
-            </div>
-          )}
-          <div>{cards.length === 0 ? "WOOPS! no card" : ""}</div>
-        </div>
-        <div className="chain-view">
-          {/* <Scrollbars style={{width:300,height:600,color:"yellow !important" }}
-                 > */}
-          <div class="scrollbar" id="style-1">
-            <div class="force-overflow">
-              <div>{cards}</div>
-            </div>
-          </div>
 
-          {/* </Scrollbars> */}
+            {/* </Scrollbars> */}
+          </div>
         </div>
       </div>
     );
@@ -109,7 +111,7 @@ class DetailViewCard extends React.Component {
         <div className="text-card" style={{ padding: 20 }}>
           {/* fbf5f3  */}
           <Scrollbars
-            style={{ width: 640, height: 540, backgroundColor: "#e8e6a3" }}
+            style={{ width: 500, height: 400, backgroundColor: "#e8e6a3" }}
           >
             <div
               style={{ padding: 20 }}
